2db141ffd2e1615fa117d6aaeef8e3ff60132007
Diogo Soares <diogosoares1997@gmail.com>
2018-11-28 17:00:11 +0000

    Started Waiting Lines - Erro no transport_line_idx

---
+++ Modelo de chamadas em espera/expon.m
@@ -0,0 +1,5 @@
+% exponential number generation
+function r=expon(avg, N)
+
+	r=-avg * log(rand(1,N));
+end
---
+++ Modelo de chamadas em espera/setup.m
@@ -0,0 +1,48 @@
+function [updated_wLinhas,updated_Linhas, transport_line_idx, updated_state_vars]=setup(Fila_espera,Linhas, idx, duracao, state_vars, config_vars)
+
+    % SETUP (Inicio de chamada)
+    state_vars.totalCalls=state_vars.totalCalls+1;
+    state_vars.reqServiceTime=state_vars.reqServiceTime+duracao;
+    if (state_vars.occupiedLines >= config_vars.nLines)
+        if(state_vars.waitOccupiedLines < config_vars.wLines)
+            % Chamada em fila de espera
+            state_vars.waitOccupiedLines=state_vars.waitOccupiedLines+1;
+            for idx_waitLine =1:config_vars.wLines
+                if (Fila_espera(idx_waitLine)==0)
+                Fila_espera(idx_waitLine)=idx;
+                transport_line_idx=0;
+                end
+            end
+        end
+    else
+        m = min(Fila_espera(Fila_espera>0));
+        if(m ~= 0)
+        % Chamada servida da fila de espera
+        state_vars.occupiedLines=state_vars.occupiedLines+1;
+        for idx_line=1: config_vars.nLines
+            if (Linhas(idx_line)==0)
+                Linhas(idx_line)=m;
+                transport_line_idx=idx_line;
+                Fila_espera(m)=0;
+                state_vars.waitOccupiedLines=state_vars.waitOccupiedLines-1;
+                break;
+            end
+        end
+        else
+        % Chamada servida
+        state_vars.occupiedLines=state_vars.occupiedLines+1;
+        for idx_line=1: config_vars.nLines
+            if (Linhas(idx_line) == 0)
+                Linhas(idx_line)=idx;
+                %tlinha(idx)=idx_linha;
+                transport_line_idx=idx_line;
+                break;
+            end
+        end
+       state_vars.carriedServiceTime=state_vars.carriedServiceTime+duracao;
+        end
+    end
+    updated_state_vars=state_vars;
+    updated_Linhas=Linhas;
+    updated_wLinhas=Fila_espera;
+end
